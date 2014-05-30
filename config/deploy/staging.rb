66aca0cf477ee4a98385f700f6530c26baeeaee9
Kronda <ephany@gmail.com>
2014-05-30 08:43:54 -0700

    config for staging and prod

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,18 +1,18 @@
 # Set the deployment directory on the target hosts.
-set :deploy_to, "/home/username/sites/#{application}-#{stage}"
+set :deploy_to, "/home/revolver/sites/#{application}-#{stage}"
 
 # Use the correct branch on github. Uncomment this if you have set up seperate branches for each staging area
 set :branch, "staging"
 
 # The hostnames to deploy to.
-role :web, "kronda.com"
+role :web, "208.43.141.130"
 
 # Specify one of the web servers to use for database backups or updates.
 # This server should also be running Wordpress.
-role :db, "kronda.com", :primary => true
+role :db, "208.43.141.130", :primary => true
 
 # The path to wp-cli
 set :wp, "cd #{current_path}/#{app_root} ; /usr/local/bin/wp"
 
 # The username on the target system, if different from your local username
-ssh_options[:user] = 'krondaco'
+ssh_options[:user] = 'revolver'
